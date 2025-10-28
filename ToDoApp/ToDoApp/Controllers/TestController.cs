b3015ff30d44c965223de2c0aee9c07bd5adb8e8
parsapanahpoor <parsapanahpoor77@gmail.com>
2025-10-28 21:55:11 +0330

    S3

---
+++ ToDoApp/ToDoApp/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApp.Controllers;
+
+public class TestController : Controller
+{
+    [HttpGet]
+    public IActionResult ShowSumPage(int a, string b)
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Sum1(int a , string b)
+    {
+        //Logic and business
+
+        return RedirectToAction(nameof(ShowSumPage));
+    }
+
+    [HttpGet]
+    public IActionResult Sum2(int a, string b)
+    {
+        //Logic and business
+
+        return RedirectToAction(nameof(ShowSumPage));
+    }
+}
